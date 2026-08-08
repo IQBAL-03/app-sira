6bd5a3760b1e56d79b30ca9e073f44e0c1668b4e
IQ-2 <muhamadiqbal9871@gmail.com>
2026-08-08 21:16:14 +0700

    Menambahkan table complaint, Due, LetterRequest dan user sekalian buat akun admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,15 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'nik' => '1234567890123456',
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'phone' => '085801214341',
+            'address' => 'Rt 06 / Rw 07',
+            'role' => 'admin',
+            'is_verified' => true,
+            'password' => Hash::make('Admin123')
+        ]);
     }
 }

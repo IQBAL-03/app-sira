10278c6f3651d79a01f8a0d6c3b42e815f606797
IQ-2 <muhamadiqbal9871@gmail.com>
2026-08-12 22:01:44 +0700

    Restore serverless storage setup and fix REDIS_HOST

--- api/index.php
+++ api/index.php
@@ -1,2 +1,31 @@
 <?php
+
+// Setup writable directories in /tmp for Vercel serverless
+$dirs = [
+    '/tmp/storage/app/public',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+    '/tmp/bootstrap/cache',
+];
+
+foreach ($dirs as $dir) {
+    if (!is_dir($dir)) {
+        @mkdir($dir, 0755, true);
+    }
+}
+
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/bootstrap/cache/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/bootstrap/cache/packages.php';
+$_ENV['APP_CONFIG_CACHE']   = '/tmp/bootstrap/cache/config.php';
+$_ENV['APP_ROUTES_CACHE']   = '/tmp/bootstrap/cache/routes.php';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
+
+putenv('APP_SERVICES_CACHE=/tmp/bootstrap/cache/services.php');
+putenv('APP_PACKAGES_CACHE=/tmp/bootstrap/cache/packages.php');
+putenv('APP_CONFIG_CACHE=/tmp/bootstrap/cache/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/bootstrap/cache/routes.php');
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+
 require __DIR__ . '/../public/index.php';
